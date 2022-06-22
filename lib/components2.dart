3d2421242af6446a9a54403ffdb195efe8739aca
Lam To <toxuanlam@gmail.com>
2022-06-22 10:34:42 +0700

    update

--- lib/components2.dart
+++ lib/components2.dart
@@ -7,7 +7,7 @@ export 'src/components2/fade_slide_transition.dart';
 export 'src/components2/fixed_height_progress_indicator.dart';
 export 'src/components2/fixed_item_extent_grid_view.dart';
 export 'src/components2/list_item.dart';
-export 'src/components2/misc.dart' show isCupertino;
+export 'src/components2/misc.dart' hide decidePadding;
 export 'src/components2/no_colorized_image_icon.dart';
 export 'src/components2/platform.dart';
 export 'src/components2/radio_list_item.dart';
--- lib/src/components2/misc.dart
+++ lib/src/components2/misc.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import 'breakpoint.dart';
 
@@ -23,3 +24,42 @@ bool get isCupertino {
   }
   return Platform.isIOS || Platform.isMacOS;
 }
+
+///
+/// Use FutureOr to allow the caller ignore the result
+/// The return type is always Future<SnackBarClosedReason>
+///
+FutureOr<SnackBarClosedReason> showToast(
+  BuildContext context,
+  String message, [
+  String? action,
+]) =>
+    ScaffoldMessenger.of(context)
+        .showSnackBar(
+          SnackBar(
+            content: Text(message),
+            behavior: SnackBarBehavior.floating,
+            action: action == null
+                ? null
+                : SnackBarAction(
+                    label: action,
+                    textColor: Theme.of(context).colorScheme.inversePrimary,
+                    onPressed: () {},
+                  ),
+          ),
+        )
+        .closed;
+
+extension ThemeExt on ThemeData {
+  Color applySurfaceTint(double elevation) => ElevationOverlay.applySurfaceTint(
+        colorScheme.surface,
+        colorScheme.surfaceTint,
+        elevation,
+      );
+}
+
+EdgeInsets avoidFloatingActionButtonPadding(BuildContext context) =>
+    EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 56);
+
+EdgeInsets avoidExtendedFloatingActionButtonPadding(BuildContext context) =>
+    EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 82);
