6306d26e2775f65c6969f10ed48a69c998ed9914
Lam To <toxuanlam@gmail.com>
2022-12-02 11:30:18 +0700

    addCancelable

--- lib/src/presentation/state_ext.dart
+++ lib/src/presentation/state_ext.dart
@@ -14,19 +14,30 @@ extension StateExt on BaseState {
     const key = "presentation:addSubscription";
     setTagIfAbsent(key, _SubscriptionCleaner.new).add(subscription);
   }
+
+  void addCancellable(Cancellable cancellable) {
+    const key = "presentation:addSubscription";
+    setTagIfAbsent(key, _SubscriptionCleaner.new).add(cancellable);
+  }
 }
 
 class _SubscriptionCleaner implements Cancellable {
-  final _values = <StreamSubscription<void>>[];
+  final _values = <Object>[]; // StreamSubscription<void> or Cancellable
 
-  void add(StreamSubscription<void> subscription) {
+  void add(Object subscription) {
+    assert(subscription is Cancellable ||
+        subscription is StreamSubscription<void>);
     _values.add(subscription);
   }
 
   @override
   void cancel() {
     for (final value in _values) {
-      value.cancel();
+      if (value is Cancellable) {
+        value.cancel();
+      } else if (value is StreamSubscription<void>) {
+        value.cancel();
+      }
     }
   }
 }
